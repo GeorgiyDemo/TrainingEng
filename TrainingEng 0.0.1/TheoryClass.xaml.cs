14cff29ed673916c304f9dfc1d8e8b4379daeeaf
Georgiy Demo <demenchuk.george@protonmail.com>
2020-11-27 12:17:40 +0300

    Получение текущей директории проекта

--- TrainingEng 0.0.1/TheoryClass.xaml.cs
+++ TrainingEng 0.0.1/TheoryClass.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,13 +27,19 @@ namespace TrainingEng_0._0._1
             //TheoryTwoTem1.Navigate(@"E:\TrainingEng\TrainingEng 0.0.1\TrainingEng 0.0.1\TrainingEng 0.0.1\Frame3.png");
        
             if (Globals.TheoryFail != null)
-       
             {
+
+                //Получает текущую директорию
+                String CurrentDir = CurrentDirFormater();
+
                 if (Globals.Classes == 2)
                 {
+                    //TODO
+                    //QuestionBlock.Text = SQLiteClass.SQLiteGet("SELECT question FROM Questions WHERE id=" + thisnumber.ToString());
+                    //FirstAnswerButton.Content = "A: " + SQLiteClass.SQLiteGet("SELECT first FROM Questions WHERE id=" + thisnumber.ToString());
                     if (Globals.TheoryFail == "Moi_bukvy")
                     {
-                        TheoryTwoTem1.Navigate(@"E:\TrainingEng.V2\TrainingEng 0.0.1\Theory\Moi_bukvy.html");
+                        TheoryTwoTem1.Navigate(CurrentDir+@"\Theory\Moi_bukvy.html");
                     }
                     else if (Globals.TheoryFail == "Moy_dom")
                     {
@@ -64,6 +71,16 @@ namespace TrainingEng_0._0._1
 
             //TheoryTwoTem1.Navigate(@"E:\TrainingEng\TrainingEng 0.0.1\TrainingEng 0.0.1\TrainingEng 0.0.1\V_vannoy.html");
        
+        }
+        
+        //Возвращает полный путь до корневой папки проекта
+        private String CurrentDirFormater()
+        {
+            String buffer = Directory.GetCurrentDirectory();
+            String[] words = buffer.Split('\\');
+            String[] newWords = words.Take(words.Count() - 2).ToArray();
+            return String.Join("\\", newWords);
+    
         }
 
         private void TwoThema1Practic_Click(object sender, RoutedEventArgs e)
